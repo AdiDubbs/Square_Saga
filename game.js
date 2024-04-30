ad15c0370d942098dc5f8a496fc219b362470c73
Aditya Dubey <adubey7@wisc.edu>
2024-04-30 16:17:06 -0500

    C4: Square Saga

--- game.js
+++ game.js
@@ -8,18 +8,22 @@ const BOARD_WIDTH = 400;
 const BOARD_HEIGHT = 800;
 
 let startScreen = true;
+let modeScreen = false;
 let gameScreen = false;
 let endScreen = false;
 let quitButton = false;
 let rowClear = false;
 
 let colors = ['cyan', 'purple', 'orange', 'yellow', 'green', 'blue', 'red'];
+let modeColors = ['white', 'black', 'grey', 'dimgray'];
 let tetros = [];
 let currTetro = [];
 let allSquares = [];
 let prev, curr;
 let score = 0;
 let time = 0;
+let mode = 0;
+let won = false;
 
 window.addEventListener('keydown', function(event) {
     if (event.key === 'ArrowLeft') {
@@ -39,8 +43,65 @@ window.addEventListener('keydown', function(event) {
     }
 });
 
+function designBoard() {
+    let canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("myCanvas"));
+    let context = canvas.getContext('2d');
+    if (context) {
 
-export function make() {
+        // background
+        context.fillStyle = modeColors[0];
+        context.fillRect(0, 0, BOARD_WIDTH, BOARD_HEIGHT * 7/20);
+        context.fillStyle = colors[Math.floor(Math.random() * 7)];
+        context.fillRect(0, BOARD_HEIGHT * 7/20, BOARD_WIDTH, BOARD_HEIGHT * 6/20);    
+        context.fillStyle = modeColors[3];
+        context.fillRect(0, BOARD_HEIGHT * 13/20, BOARD_WIDTH, BOARD_HEIGHT * 7/20);
+
+        context.strokeStyle = modeColors[1];
+        // strokes
+        for (let i = 0; i < 10; i++) {
+            context.beginPath();
+            context.moveTo(i * BLOCK_SIZE, 0);
+            context.lineTo(i * BLOCK_SIZE, BOARD_HEIGHT * 7/20);
+            context.stroke();
+            context.closePath();
+
+            context.beginPath();
+            context.moveTo(i * BLOCK_SIZE, BOARD_HEIGHT * 13/20);
+            context.lineTo(i * BLOCK_SIZE, BOARD_HEIGHT);
+            context.stroke();
+            context.closePath();
+
+            context.beginPath();
+            context.moveTo(i * BLOCK_SIZE, BOARD_HEIGHT * 7/20);
+            context.lineTo(i * BLOCK_SIZE, BOARD_HEIGHT * 13/20);
+            context.stroke();
+            context.closePath();
+        }
+
+        for (let j = 0; j < 7; j++) {
+            context.beginPath();
+            context.moveTo(0, j * BLOCK_SIZE);
+            context.lineTo(BOARD_WIDTH, j * BLOCK_SIZE);
+            context.stroke();
+            context.closePath();
+
+            context.beginPath();
+            context.moveTo(0, BOARD_HEIGHT * 13/20 + j * BLOCK_SIZE);
+            context.lineTo(BOARD_WIDTH, BOARD_HEIGHT * 13/20 + j * BLOCK_SIZE);
+            context.stroke();
+            context.closePath();
+        }
+        for (let k = 0; k < 6; k++) {
+            context.beginPath();
+            context.moveTo(0, BOARD_HEIGHT * 7/20 + k * BLOCK_SIZE);
+            context.lineTo(BOARD_WIDTH, BOARD_HEIGHT * 7/20 + k * BLOCK_SIZE);
+            context.stroke();
+            context.closePath();
+        }
+    }
+}
+
+function make() {
     let type = Math.floor(Math.random() * 6) + 1;
     let color = 0;
     let x = 0;
@@ -74,24 +135,15 @@ function detectCollision(x, y) {
     return false; // No collision
 }
 
-export function game() {
-
-    function block(context, x, y) {
-        context.strokeStyle = "black";
-        context.lineWidth = 2;
-        context.beginPath();
-        context.moveTo(x, y);
-        context.lineTo(x + BLOCK_SIZE, y);
-        context.lineTo(x + BLOCK_SIZE, y - BLOCK_SIZE);
-        context.lineTo(x, y - BLOCK_SIZE);
-        context.closePath();
-        context.fill();
-        context.stroke();
-    }
+function game() {
 
     function board(context) {
-        context.strokeStyle = 'grey';
-        context.fillStyle = 'black';
+        context.strokeStyle = modeColors[2];
+        if (mode == 2) {
+            context.fillStyle = modeColors[3];
+        } else {
+            context.fillStyle = modeColors[1];
+        }
         context.fillRect(0, 0, canvas.width, canvas.height)
         context.lineWidth = 0.8;
         for (let i = 0; i < 10; i++) {
@@ -104,6 +156,27 @@ export function game() {
             context.lineTo(canvas.width, i * BLOCK_SIZE);
             context.stroke();
         }
+    }
+
+    function block(context, x, y) {
+        if (mode == 0) {
+            context.strokeStyle = modeColors[1];
+        } else if (mode == 1) {
+            context.strokeStyle = modeColors[2];
+            context.fillStyle = modeColors[0];
+        } else if (mode == 2) {
+            context.strokeStyle = modeColors[2];
+            context.fillStyle = modeColors[1];
+        }
+        context.lineWidth = 2;
+        context.beginPath();
+        context.moveTo(x, y);
+        context.lineTo(x + BLOCK_SIZE, y);
+        context.lineTo(x + BLOCK_SIZE, y - BLOCK_SIZE);
+        context.lineTo(x, y - BLOCK_SIZE);
+        context.closePath();
+        context.fill();
+        context.stroke();
     }
 
     function pieces(type, x, y, color, rot) {
@@ -423,24 +496,132 @@ function menus() {
 
     function startMenu(context) {
 
+        designBoard();
         context.fillStyle = 'black';
         context.fillRect(0, 0, canvas.width, canvas.height);
+        context.font = '800 100px Courier New';
         context.fillStyle = 'white';
-        context.font = '800 90px Courier New';
-        context.fillText("Shape", 40, 200);
-        context.fillText("Shift", 80, 300);
+        context.fillText("Square", 130, 200);
+        context.fillStyle = 'red';
+        context.fillText("Saga", 220, 300);
+
+        context.fillStyle = 'white';
+        context.font = '550 20px Courier New';
+        let text = "A blocky tale of light and darkness";
+        context.fillText(text, 100, 400);
+        context.textAlign = "center";
 
         // Create start button
         let startButton = document.createElement("startButton");
         startButton = createButton("Start", startButton);
         startButton.onclick = function () {
-            gameScreen = true;
+            modeScreen = true;
+            //gameScreen = true;
             document.body.removeChild(startButton);
-            make();
-            game();
+            //make();
+            //game();
         }
         document.body.appendChild(startButton);
     }
+
+    function modeMenu(context) {
+        
+        designBoard();
+
+        context.fillStyle = 'black';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        context.fillStyle = 'white';
+        context.font = '550 15px Courier New';
+        let lines = [
+            "In an alternate universe, a fierce battle is raging",
+            "between the forces of light and darkness.",
+            "Amidst the chaos, a new hero emerges - you.",
+            "",
+            "Will you side with the forces of light, and fight for justice?",
+            "Or will you succumb to darkness, and seek to conquer the world?",
+            "Maybe you'll choose the path of neutrality,",
+            "The fate of the universe hangs in the balance,",
+            "awaiting your decision ..."
+        ];
+        let lineHeight = 50;
+        let y = 100;
+        for (let i = 0; i < lines.length; i++) {
+            context.textAlign = "center";
+            context.fillText(lines[i], canvas.width / 2, y);
+            y += lineHeight;
+        }
+
+        let lightButton = document.createElement("lightButton");
+        lightButton.style.width = "150px";
+        lightButton.style.height = "30px";
+        lightButton.innerHTML = "Light";
+        lightButton.style.fontSize = "25px";
+        lightButton.style.padding = "2px";
+        lightButton.style.textAlign = "center";
+        lightButton.style.borderRadius = "10px";
+        lightButton.style.backgroundColor = "white";
+        lightButton.style.color = "black";
+        lightButton.style.position = "absolute";
+        lightButton.style.left = "530px";
+        lightButton.style.top = "600px";
+        document.body.appendChild(lightButton);
+        
+        let darkButton = document.createElement("darkButton");
+        darkButton.style.width = "150px";
+        darkButton.style.height = "30px";
+        darkButton.innerHTML = "Dark";
+        darkButton.style.fontSize = "25px";
+        darkButton.style.padding = "2px";
+        darkButton.style.textAlign = "center";
+        darkButton.style.borderRadius = "10px";
+        darkButton.style.backgroundColor = "grey";
+        darkButton.style.color = "black";
+        darkButton.style.position = "absolute";
+        darkButton.style.left = "760px";
+        darkButton.style.top = "600px";
+        document.body.appendChild(darkButton);
+
+        let classicButton = document.createElement("classicButton");
+        classicButton.style.width = "150px";
+        classicButton.style.height = "30px";
+        classicButton.innerHTML = "Classic";
+        classicButton.style.fontSize = "25px";
+        classicButton.style.padding = "2px";
+        classicButton.style.textAlign = "center";
+        classicButton.style.borderRadius = "10px";
+        classicButton.style.backgroundColor = "blue";
+        classicButton.style.color = "white";
+        classicButton.style.position = "absolute";
+        classicButton.style.left = "660px";
+        classicButton.style.top = "680px";
+        document.body.appendChild(classicButton);
+
+        lightButton.onclick = function () {
+            mode = 1;
+            buttonClick();
+        }
+
+        darkButton.onclick = function () {
+            mode = 2;
+            buttonClick();
+        }
+
+        classicButton.onclick = function () {
+            mode = 0;
+            buttonClick();
+        }
+
+        function buttonClick() {
+            gameScreen = true;
+            modeScreen = false;
+            document.body.removeChild(lightButton);
+            document.body.removeChild(darkButton);
+            document.body.removeChild(classicButton);
+            make();
+            game();
+        }
+    }
+    
 
     function gameplayMenu(context) {
         
@@ -449,19 +630,19 @@ function menus() {
             
             context.fillStyle = 'white';
             context.font = '600 40px Courier New';
-            context.fillText("SCORE", 190, 100);
+            context.fillText("SCORE", 300, 100);
             context.textAlign = "center";
 
             // Create score display
             context.fillStyle = 'white';
             context.font = '400 100px Courier New';
-            context.fillText(score, 190, 200);
+            context.fillText(score, 300, 200);
             context.textAlign = "center";
 
             if (rowClear && time < 50) {
                 context.fillStyle = 'white';
                 context.font = '550 30px Courier New';
-                context.fillText("Row Cleared!", 200, 350);
+                context.fillText("Row Cleared!", 300, 350);
                 context.textAlign = "center";
                 time++;
             }
@@ -502,15 +683,45 @@ function menus() {
 
     function endMenu(context) {
         context.fillStyle = 'black';
-        context.fillRect(0, 0, canvas.width, canvas.height)
+        context.fillRect(0, 0, canvas.width, canvas.height);
 
-        context.fillStyle = 'white';
-        context.font = '600 100px Courier New';
-        context.fillText("GAME", 200, 200);
-        context.fillText("OVER", 200, 300);
+        if (score > 0) {
+            if (mode == 1) {
+                context.fillStyle = 'white';
+                context.font = '540 25px Courier New';
+                context.fillText("You have chosen the path of light", 300, 400);
+                context.fillText("and have saved the universe.", 300, 450);
+            }
+            if (mode == 2) {
+                context.fillStyle = 'red';
+                context.font = '540 25px Courier New';
+                context.fillText("You have chosen the path of darkness", 300, 400);
+                context.fillText("and have conquered the universe.", 300, 450);
+            }
+            context.font = '600 50px Courier New';
+            context.fillText("You Won!", 300, 250);
+            context.textAlign = "center";
+        } else {
+            if (mode == 1) {
+                context.fillStyle = 'white';
+                context.font = '540 25px Courier New';
+                context.fillText("You had chosen the path of light", 300, 400);
+                context.fillText("but the darkness has won.", 300, 450);
+                context.fillText("The world has been destroyed.", 300, 500);
+            } else if (mode == 2) {
+                context.fillStyle = 'red';
+                context.font = '540 25px Courier New';
+                context.fillText("You had chosen the path of darkness", 300, 400);
+                context.fillText("but the light proved too strong.", 300, 450);
+                context.fillText("The darkness has been vanquished.", 300, 500);
+            }
+            context.font = '600 50px Courier New';
+            context.fillText("You Lost ...", 300, 250);
+            context.textAlign = "center";
+        }
 
-        context.font = '400 30px Courier New';
-        context.fillText("Your score: " + score, 200, 600);
+        context.font = '600 25px Courier New';
+        context.fillText("Your score: " + score, 300, 600);
         context.textAlign = "center";
     }
 
@@ -526,7 +737,7 @@ function menus() {
         button.style.backgroundColor = "red";
         button.style.color = "white";
         button.style.position = "absolute";
-        button.style.left = "560px";
+        button.style.left = "670px";
         button.style.top = "600px";
         return button;
     }
@@ -537,6 +748,9 @@ function menus() {
     if (startScreen) {
         startScreen = false;
         startMenu(context);
+    } else if (modeScreen) {
+        modeScreen = false;
+        modeMenu(context);
     } else if (gameScreen) {
         gameplayMenu(context);
     } else if (endScreen) {
